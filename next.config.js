383235f3761d6b44f737df0710f4ddccb8e98408
Jorge Grimaldi MSFT <jgrimaldi@microsoft.com>
2025-07-25 15:48:30 -0600

    nit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": require("path").resolve(__dirname, "src"),
-    };
-    return config;
-  }
 };
 
 module.exports = nextConfig;
