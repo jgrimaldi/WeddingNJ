7005f8d2ddc38bd5b1b18dc9a5ebe916c76aea51
Jorge Grimaldi MSFT <jgrimaldi@microsoft.com>
2025-07-25 14:53:21 -0600

    remove globals.css

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": require("path").resolve(__dirname, "src"),
-    };
-    return config;
-  }
 };
 
 module.exports = nextConfig;
